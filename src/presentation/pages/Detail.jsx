237114deb4773ce5e48783dcee496a40ce3d1f2b
yuninam2128 <162120020+yuninam2128@users.noreply.github.com>
2025-10-12 23:19:05 +0900

    💄 Style : sidebar css 수정
    sidebar의 css를 figma와 일치하도록 수정하였다.

--- src/presentation/pages/Detail.jsx
+++ src/presentation/pages/Detail.jsx
@@ -252,21 +252,25 @@ function ProjectDetail() {
         return <div className="loading-container"><p>프로젝트를 불러오는 중...</p></div>;
     }
 
+    //오늘 날짜 출력 
+        const today = new Date();
+        const formatted = today.toLocaleDateString("ko-KR", {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+        });
+
 
   return (
     <div className="App">
     <div className="body-detail">
         <div className="container-detail">
-            {/* 이 부분을 '지우고' sidebar컴포넌트를 넣는다. */}
-            {/* 단, sidebar 컴포넌트 전체를 감싼 div태그 className은 무조건 sidebar로 할 것 */}
-            <div className="sidebar-detail">
                 <Sidebar />
-            </div>
 
             <div className="main-wrapper-detail">
                 <Header onAddClick={handleAddClick}/>    
                 <article className="main-article-detail">
-                        <div className="date-detail">2025년 09월 10일</div>
+                        <div className="date-detail">{formatted}</div>
                         <div className="title-detail">
                             <span className="highlight-detail">{project.title}</span>의 행성들을 정복해보아요!
                         </div>
@@ -285,12 +289,7 @@ function ProjectDetail() {
                         subtask={selectedSubtask}
                         onUpdateSubtask={handleEditSubtask}
                     />
-                    {/* <section className="main-content"></section> */}
-                    {/* <section className="todo-bar"></section> */}
                 </main>
-
-                {/* 마찬가지로 이 부분을 '지우고' 타임라인 컴포넌트를 넣는다. */}
-                {/* 단, timeline 컴포넌트 전체를 감싼 div태그 className은 무조건 timeline로 할 것 */}
                 <footer className="timeline-detail">
                     <ProjectTimeline />
                 </footer>
